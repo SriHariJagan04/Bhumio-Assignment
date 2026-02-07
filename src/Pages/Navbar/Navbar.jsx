885f00d15a37e77933ca32518e2cdb01ea9ab6f7
Sri Hari Jagan <sriharijagan333@gmail.com>
2026-02-07 06:39:28 +0530

    Add files via upload

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { FiMenu, FiX } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false);
-
-  const toggleMenu = () => setIsOpen(!isOpen);
 
   return (
     <nav className={styles.navbar}>
       <div className={styles.logo}>Bhumio Assignment</div>
 
       <div className={`${styles.navLinks} ${isOpen ? styles.active : ""}`}>
-        <a href="/">Eventually Consistent Form</a>
-        <a href="/OutOfOrderEvents">Out Of Order Events</a>
-        <a href="/ValidationThatLies">Validation That Lies</a>
+        <Link to="/" onClick={() => setIsOpen(false)}>
+          Eventually Consistent Form
+        </Link>
+
+        <Link to="/OutOfOrderEvents" onClick={() => setIsOpen(false)}>
+          Out Of Order Events
+        </Link>
+
+        <Link to="/ValidationThatLies" onClick={() => setIsOpen(false)}>
+          Validation That Lies
+        </Link>
       </div>
 
-      <div className={styles.menuIcon} onClick={toggleMenu}>
+      <div className={styles.menuIcon} onClick={() => setIsOpen(!isOpen)}>
         {isOpen ? <FiX size={24} /> : <FiMenu size={24} />}
       </div>
     </nav>
